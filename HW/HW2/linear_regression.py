e9cb46384e3543b29e662f2f3b590548083a23b1
Shayan Rahimi <1shayanrsh@gmail.com>
2021-12-13 18:12:46 +0300

    second try

--- HW/HW2/linear_regression.py
+++ HW/HW2/linear_regression.py
@@ -4,10 +4,25 @@ from numpy.linalg import inv
 import matplotlib.pyplot as plt
 
 
-def linear_regression(x, y):
-    left_term = np.dot(x.T, x)
-    left_term = inv(left_term)
-    middle_term = np.dot(left_term, x.T)
-    beta_hat = np.dot(middle_term, y)
+def linear_regression(x=None, y=None, data_set=None):
+    df = pd.read_csv(data_set, nrows=1000)
+    if data_set == 'bottle.csv':
+        x = df['T_degC']
+        y = df['Salnty']
+    else:
+        x = df.columns(0)
+        y = df.columns(1)
 
-    return beta_hat
+    # left_term = np.dot(x.T, x)
+    # left_term = inv(left_term)
+    # middle_term = np.dot(left_term, x.T)
+    # beta_hat = np.dot(middle_term, y)
+
+    plt.scatter(x, y)
+    plt.show()
+
+    regression_estimates = []
+    standard_errors = []
+    credible_intervals = []
+
+    return regression_estimates, standard_errors, credible_intervals
--- HW/HW2/test_linear_regression.py
+++ HW/HW2/test_linear_regression.py
@@ -1,7 +1,4 @@
 from linear_regression import *
 
-x = np.array([[1, 2], [3, 4]])
-y = x
 
-a = linear_regression(x, y)
-print(a)
+
